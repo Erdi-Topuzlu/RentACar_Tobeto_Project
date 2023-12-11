4fcb3e523bd8fadc5b3a0eeb2065ee85b9d54a2c
halilkrkn <halilkrkn000@gmail.com>
2023-12-11 12:43:20 +0300

    Added Model and User Entities

--- src/main/java/com/tobeto/RentACar/entities/Model.java
+++ src/main/java/com/tobeto/RentACar/entities/Model.java
@@ -1,4 +1,19 @@
 package com.tobeto.RentACar.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Table(name = "Models")
+@Data
+@Entity
 public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
 }
--- src/main/java/com/tobeto/RentACar/entities/User.java
+++ src/main/java/com/tobeto/RentACar/entities/User.java
@@ -1,4 +1,27 @@
 package com.tobeto.RentACar.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Table(name = "Users")
+@Data
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "datetime")
+    private LocalDate BirthDate;
+
 }
