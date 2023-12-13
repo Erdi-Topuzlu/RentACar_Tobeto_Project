91d28adfcf88a73af4d38ec573346987122bc177
halilkrkn <halilkrkn000@gmail.com>
2023-12-13 13:13:35 +0300

    Fixed GetAllColorResponse and GetByIdColorResponse

--- src/main/java/com/tobeto/RentACar/services/dtos/responses/color/GetAllColorResponse.java
+++ src/main/java/com/tobeto/RentACar/services/dtos/responses/color/GetAllColorResponse.java
@@ -10,5 +10,4 @@ import lombok.NoArgsConstructor;
 public class GetAllColorResponse {
     private Integer id;
     private String colorName;
-    private Integer carId;
 }
--- src/main/java/com/tobeto/RentACar/services/dtos/responses/color/GetByIdColorResponse.java
+++ src/main/java/com/tobeto/RentACar/services/dtos/responses/color/GetByIdColorResponse.java
@@ -10,5 +10,4 @@ import lombok.NoArgsConstructor;
 public class GetByIdColorResponse {
     private Integer id;
     private String colorName;
-    private Integer carId;
 }
