244a4851da14fc9062a94de3b5bfe1094a78595d
Erdi Topuzlu <erditpzl@gmail.com>
2023-12-15 13:33:19 +0300

    Deleted Unnecessary Import in AddBrandRequest

--- src/main/java/com/tobeto/RentACar/services/dtos/requests/brand/AddBrandRequest.java
+++ src/main/java/com/tobeto/RentACar/services/dtos/requests/brand/AddBrandRequest.java
@@ -1,7 +1,6 @@
 package com.tobeto.RentACar.services.dtos.requests.brand;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
