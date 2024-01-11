de6c7ad0c2ac1ed0a97e6ed1b802a38d00b3b375
Erdi Topuzlu <erditpzl@gmail.com>
2024-01-11 15:07:36 +0300

    .

--- frontend/rentACar/src/App.jsx
+++ frontend/rentACar/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <div>Pair-1</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
