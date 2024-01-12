68fc0cf80dcaa4cef799bf0d126245ee4b1eb48e
Erdi Topuzlu <erditpzl@gmail.com>
2024-01-12 20:37:50 +0300

    .

--- frontend/rentACar/src/main.jsx
+++ frontend/rentACar/src/main.jsx
@@ -50,9 +50,8 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ 
     <Provider store={store}>
     <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>
 );
