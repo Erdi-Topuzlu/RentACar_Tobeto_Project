3969a30cb8757adc481eed6171576781e6454dd9
Erdi Topuzlu <erditpzl@gmail.com>
2024-01-14 01:24:52 +0300

    Added NotFound Image

--- frontend/rentACar/src/pages/NotFound.jsx
+++ frontend/rentACar/src/pages/NotFound.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Helmet from "../components/Helmet";
+import { Col, Container, Row } from "reactstrap";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+
+import img404 from "../assets/all-images/404.jpg";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Helmet title="Not Found">
+    <Header/>
+    
+    <section>
+        <Container className="d-flex justify-content-center">
+        <img width={350} src={img404} />
+        </Container>
+      </section>
+
+   
+    <Footer/>
+  </Helmet> 
+  )
 };
 
 export default NotFound;
