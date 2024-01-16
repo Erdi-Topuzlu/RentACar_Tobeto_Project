cfb94d9024bd84408812f613c8310052ec911812
Halismelih1 <hhlsabkk@icloud.com>
2024-01-16 22:06:31 +0300

    Formik added for Login and contact pages

--- frontend/rentACar/src/pages/Contact.jsx
+++ frontend/rentACar/src/pages/Contact.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+import { Link,Form } from "react-router-dom";
+import { Container, Row, Col, FormGroup, Input, FormFeedback } from "reactstrap";
 import Helmet from "../components/Helmet";
 import CommonSection from "../components/ui/CommonSection";
-
 import "../styles/contact.css";
+import { contactValidationSchema } from "../schemes/contactScheme";
+import { useFormik } from "formik";
 
 const socialLinks = [
   {
@@ -26,6 +27,21 @@ const socialLinks = [
 ];
 
 const Contact = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      message: ""
+    },
+    validationSchema: contactValidationSchema,
+    onSubmit: (values,actions) => {
+      alert(JSON.stringify(values, null, 2));
+      actions.resetForm();
+      
+    },
+  });
+
   return (
     <Helmet title="Contact">
       <CommonSection title="Contact" />
@@ -35,22 +51,80 @@ const Contact = () => {
             <Col lg="7" md="7">
               <h6 className="fw-bold mb-4">Get In Touch</h6>
 
-              <Form>
+              <Form onSubmit={formik.handleSubmit}>
+              
+                <div>
                 <FormGroup className="contact__form">
-                  <Input placeholder="Your Name" type="text" />
-                </FormGroup>
-                <FormGroup className="contact__form">
-                  <Input placeholder="Email" type="email" />
-                </FormGroup>
-                <FormGroup className="contact__form">
-                  <textarea
-                    rows="5"
-                    placeholder="Message"
-                    className="textarea"
-                  ></textarea>
-                </FormGroup>
+                 
+                 <Input
+ 
+                           id="asd"
+                           name="name"
+                           value={formik.values.name}
+                           className={formik.errors.name && formik.touched.name && "error"}
+                           onChange={formik.handleChange}
+                           onBlur={formik.handleBlur}
+                           type="text"
+                           placeholder="Your Name"
+                           invalid={formik.touched.name && formik.errors.name}
+                           
+                         />
+                         {formik.errors.name && formik.touched.name && (
+ 
+                           <FormFeedback >
+                             <p className="text-danger"> {formik.errors.name}</p>
+                           </FormFeedback>
+                         )}
+                 </FormGroup>
+                    </div>
+                    <div>
+                    <FormGroup className="contact__form">
+                <Input
+                          id="dfg"
+                          name="email"
+                          value={formik.values.email}
+                          className={formik.errors.email && formik.touched.email && "error"}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          type="text"
+                          placeholder="E-mail"
+                          invalid={formik.touched.email && formik.errors.email}
+                          
+                        />
+                        {formik.errors.email && formik.touched.email && (
 
-                <button className=" contact__btn" type="submit">
+                          <FormFeedback >
+                            <p className="text-danger"> {formik.errors.email}</p>
+                          </FormFeedback>
+                        )}
+                </FormGroup>
+                    </div>
+                
+                <div>
+                <FormGroup className="contact__form">
+                <Input
+                          id="tyu"
+                          name="message"
+                          value={formik.values.message}
+                          className={formik.errors.message && formik.touched.message && "error"}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          type="text"
+                          placeholder="Type your message.."
+                          invalid={formik.touched.message && formik.errors.message}
+                          
+                        />
+                        {formik.errors.message && formik.touched.message && (
+
+                          <FormFeedback >
+                            <p className="text-danger"> {formik.errors.message}</p>
+                          </FormFeedback>
+                        )}
+                  
+                </FormGroup>
+                </div>
+              
+                <button disabled={formik.isSubmitting} className=" contact__btn" type="submit">
                   Send Message
                 </button>
               </Form>
--- frontend/rentACar/src/pages/Login.jsx
+++ frontend/rentACar/src/pages/Login.jsx
@@ -1,84 +1,141 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+import { Container, Row, Col, Form, FormGroup, Input, FormFeedback, Button, Label } from "reactstrap";
 import Helmet from "../components/Helmet";
-import CommonSection from "../components/ui/CommonSection";
+import "../styles/form.css";
+import { useFormik } from "formik";
+import { loginValidationSchema } from "../schemes/loginScheme";
 
-import "../styles/contact.css";
-
-const socialLinks = [
-  {
-    url: "#",
-    icon: "ri-facebook-line",
-  },
-  {
-    url: "#",
-    icon: "ri-instagram-line",
-  },
-  {
-    url: "#",
-    icon: "ri-linkedin-line",
-  },
-  {
-    url: "#",
-    icon: "ri-twitter-line",
-  },
-];
 
 const Login = () => {
+
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: loginValidationSchema,
+    onSubmit: (values,actions) => {
+      alert(JSON.stringify(values, null, 2));
+      actions.resetForm();
+
+    },
+  });
+
+
   return (
     <Helmet title="Login">
-    <section>
-      <Container>
-        <Row>
-          <Col lg="12" className="mb-5 text-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="36"
-              height="36"
-              fill="rgba(43,8,104,1)"
-            >
-              <path d="M18 8H20C20.5523 8 21 8.44772 21 9V21C21 21.5523 20.5523 22 20 22H4C3.44772 22 3 21.5523 3 21V9C3 8.44772 3.44772 8 4 8H6V7C6 3.68629 8.68629 1 12 1C15.3137 1 18 3.68629 18 7V8ZM16 8V7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V8H16ZM7 11V13H9V11H7ZM7 14V16H9V14H7ZM7 17V19H9V17H7Z"></path>
-            </svg>
-            <h2 className="section__title">Login</h2>
-            <div className="d-flex justify-content-center align-items-center">
-              <Col lg="4" className="mb-5 text-center">
-                <Form>
-                  <FormGroup>
-                    <Input placeholder="Email" type="email" />
-                  </FormGroup>
-                  <FormGroup>
-                    <Input placeholder="Password" type="password" />
-                  </FormGroup>
+      <section >
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="36"
+                height="36"
+                fill="rgba(43,8,104,1)"
+              >
+                <path d="M4 22C4 17.5817 7.58172 14 12 14C16.4183 14 20 17.5817 20 22H18C18 18.6863 15.3137 16 12 16C8.68629 16 6 18.6863 6 22H4ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM12 11C14.21 11 16 9.21 16 7C16 4.79 14.21 3 12 3C9.79 3 8 4.79 8 7C8 9.21 9.79 11 12 11Z"></path>
+              </svg>
+              <h2 className="section__title">Login</h2>
+              <div className="d-flex justify-content-center align-items-center">
+                <Col lg="4" className="mb-5 text-center">
 
-                  <FormGroup>
-                    <div className="d-flex flex-column-reverse flex-lg-row justify-content-between align-items-center">
-                      <Link
-                        to="/home"
-                        className="d-flex align-items-center gap-2 mb-2 mb-lg-0"
-                      >
-                        Forgot Password?
-                      </Link>
-                      <Link
-                        to="/sign-up"
-                        className="d-flex align-items-center gap-2"
-                      >
-                        Don't have an Account? Sign Up
-                      </Link>
+
+
+                  <Form onSubmit={formik.handleSubmit}>
+
+                    <div>
+                      <FormGroup className="">
+                        <Input
+                          id="email"
+                          name="email"
+                          value={formik.values.email}
+                          className={formik.errors.email && formik.touched.email && "error"}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          type="text"
+                          placeholder="E-mail"
+                          invalid={formik.touched.email && formik.errors.email}
+                        />
+                        {formik.errors.email && formik.touched.email && (
+
+                          <FormFeedback >
+                            <p className="text-danger"> {formik.errors.email}</p>
+                          </FormFeedback>
+                        )}
+
+
+                      </FormGroup>
                     </div>
-                  </FormGroup>
+                    <div>
+                      <FormGroup>
+                        <Input
+                          id="pw"
+                          name="password"
+                          value={formik.values.password}
+                          className={formik.errors.password && formik.touched.password && "error"}
+                          onChange={formik.handleChange}
+                          onBlur={formik.handleBlur}
+                          type="password"
+                          placeholder="Password"
+                          invalid={formik.touched.password && formik.errors.password}
 
-                  <button className=" contact__btn" type="submit">
-                    Login
-                  </button>
-                </Form>
-              </Col>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+                        />
+                        {formik.errors.password && formik.touched.password && (
+                          <FormFeedback >
+                            <p className="text-danger"> {formik.errors.password}</p>
+                          </FormFeedback>
+                        )}
+                      </FormGroup>
+
+                    </div>
+                    <FormGroup className="d-flex justify-content-end"
+                      check
+                      inline
+                    >
+                      <Input type="checkbox" />
+                      <Label style={{marginLeft:4}}  check>
+                         Remember me
+                      </Label>
+                    </FormGroup>
+                    <FormGroup
+                      check
+                      inline
+                    >
+    
+                    </FormGroup>
+                    <FormGroup>
+                      <div className="d-flex flex-column-reverse flex-lg-row justify-content-between align-items-center">
+                        <Link
+                          to="/home"
+                          className="d-flex align-items-center gap-2 mb-2 mb-lg-0"
+                        >
+                          Forgot Password?
+                        </Link>
+                        <Link
+                          to="/sign-up"
+                          className="d-flex align-items-center gap-2"
+                        >
+                          Don't have an Account? Sign Up
+                        </Link>
+                      </div>
+                    </FormGroup>
+
+
+                    <Button disabled={formik.isSubmitting} className=" form__btn" type="submit">
+                      Login
+                    </Button>
+                  </Form>
+                </Col>
+
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   );
 };
