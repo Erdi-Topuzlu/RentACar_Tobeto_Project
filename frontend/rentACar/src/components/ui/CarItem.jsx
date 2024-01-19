4488e90b6c11f6039480db0ac5cef350974528c3
Erdi Topuzlu <erditpzl@gmail.com>
2024-01-20 00:27:13 +0300

    Added Filter in CarListing

--- frontend/rentACar/src/components/ui/CarItem.jsx
+++ frontend/rentACar/src/components/ui/CarItem.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Col } from "reactstrap";
+import { Col, Container } from "reactstrap";
 import { Link } from "react-router-dom";
 import "../../styles/car-item.css";
 import { useTranslation } from "react-i18next";
 import { vehicleType } from "../helper/conversionType";
+import { Row } from "react-bootstrap";
 
 const CarItem = ({ item }) => {
   const { t } = useTranslation();
   return (
     <Col lg="4" md="4" sm="6" className="mb-5">
-      <div className="car__item">
+      <div className="car__item" style={{ width: "auto", height: "100%" }}>
         <div className="car__img">
           <img
             src={"https://source.unsplash.com/random?wallpapers/?car"}
@@ -19,28 +20,54 @@ const CarItem = ({ item }) => {
         </div>
 
         <div className="car__item-content mt-4 text-center">
-  <h4  className="section__title">{item.modelName}</h4>
-  <h6 className="rent__price mt-2">
-    <span style={{ fontSize: '24px' }} className="text-dark">{item.dailyPrice}.00 ₺</span>{" "}
-    <span className="text-muted">/ {t("day")}</span>
-  </h6>
+          <h4 className="section__title">{item.modelName}</h4>
+          <h6 className="rent__price mt-2">
+            <span style={{ fontSize: "24px" }} className="text-dark">
+              {item.dailyPrice}.00 ₺
+            </span>{" "}
+            <span className="text-muted">/ {t("day")}</span>
+          </h6>
 
-
-          {/* <div className="car__item-info d-flex flex-column-reverse flex-lg-row align-items-center justify-content-between mt-3 mb-4">
+          {/*DOWN ICONS */}
+          <Row>
+          <Col lg="2"></Col>
+          <Col lg="8">
+          <div
+            className="car__item-content d-flex align-items-center justify-content-between" 
+            
+          >
+            <span className="d-flex align-center gap-1 pb-3 section__description">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="32"
+                        height="32"
+                        fill="rgba(249,168,38,1)"
+                      >
+                        <path d="M3 19V5.70046C3 5.27995 3.26307 4.90437 3.65826 4.76067L13.3291 1.24398C13.5886 1.14961 13.8755 1.28349 13.9699 1.54301C13.9898 1.59778 14 1.65561 14 1.71388V6.6667L20.3162 8.77211C20.7246 8.90822 21 9.29036 21 9.72079V19H23V21H1V19H3ZM5 19H12V3.85543L5 6.40089V19ZM19 19V10.4416L14 8.77488V19H19Z"></path>
+                      </svg>{" "}
+                      {item.modelId?.brandId?.name}
+                    </span>
+            <span className=" d-flex  align-center gap-1 pb-3 section__description">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="32"
+                        height="32"
+                        fill="rgba(249,168,38,1)"
+                      >
+                        <path d="M9 1V3H15V1H17V3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H7V1H9ZM20 11H4V19H20V11ZM7 5H4V9H20V5H17V7H15V5H9V7H7V5Z"></path>
+                      </svg>{" "}
+                      {item.year}
+                    </span>
+          </div>
+         
           
-            <span className="d-flex align-items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="32"
-                height="32"
-                fill="rgba(249,168,38,1)"
-              >
-                <path d="M3 19V5.70046C3 5.27995 3.26307 4.90437 3.65826 4.76067L13.3291 1.24398C13.5886 1.14961 13.8755 1.28349 13.9699 1.54301C13.9898 1.59778 14 1.65561 14 1.71388V6.6667L20.3162 8.77211C20.7246 8.90822 21 9.29036 21 9.72079V19H23V21H1V19H3ZM5 19H12V3.85543L5 6.40089V19ZM19 19V10.4416L14 8.77488V19H19Z"></path>
-              </svg>{" "}
-              {item.modelId?.brandId?.name}
-            </span>
-            <span className="d-flex align-items-center gap-1">
+          <div
+            className="car__item-content d-flex align-items-center justify-content-between"
+            
+          >
+            <span className=" d-flex  align-center gap-1 pb-3 section__description">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -52,28 +79,28 @@ const CarItem = ({ item }) => {
               </svg>{" "}
               {item.modelId?.name}
             </span>
-            <span className="d-flex align-items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="32"
-                height="32"
-                fill="rgba(249,168,38,1)"
-              >
-                <path d="M9 1V3H15V1H17V3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H7V1H9ZM20 11H4V19H20V11ZM7 5H4V9H20V5H17V7H15V5H9V7H7V5Z"></path>
-              </svg>{" "}
-              {item.year}
-            </span>
-          </div> */}
-
-          
-
-
-
-          <div className="row d-flex align-center p-2 justify-content-evenly ">
-            {/* Left */}
+           
+                    <span className=" d-flex  align-center gap-1 pb-3 section__description">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="32"
+                        height="32"
+                        fill="rgba(249,168,38,1)"
+                      >
+                        <path d="M14.1716 3C14.702 3 15.2107 3.21071 15.5858 3.58579L20.4142 8.41421C20.7893 8.78929 21 9.29799 21 9.82843V17H23V19L14.8738 19.0008C14.4295 20.7256 12.8636 22 11 22C9.13643 22 7.57052 20.7256 7.12621 19.0008L3 19C2.44772 19 2 18.5523 2 18V5C2 3.89543 2.89543 3 4 3H14.1716ZM11 16C9.89543 16 9 16.8954 9 18C9 19.1046 9.89543 20 11 20C12.1046 20 13 19.1046 13 18C13 16.8954 12.1046 16 11 16ZM14.1716 5H4V17L7.12595 17.0002C7.56991 15.2749 9.13607 14 11 14C12.8639 14 14.4301 15.2749 14.874 17.0002L19 17V9.82843L14.1716 5ZM14 7V13H6V7H14ZM12 9H8V11H12V9Z"></path>
+                      </svg>
+                      {vehicleType(item.vehicleType, t)}
+                    </span>
+          </div>
+          </Col>
+          <Col lg="2"></Col>
+          </Row>
+            {/* <Col lg="12">
+          <div className="row d-flex align-center p-2 justify-content-evenly">
+            
             <div className="col-4 p-2">
-              <span className="d-flex  align-center gap-1 pb-3 pb-3 section__description">
+              <span className="d-flex align-center gap-1 pb-3 section__description">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -99,10 +126,8 @@ const CarItem = ({ item }) => {
                 {item.year}
               </span>
             </div>
-
-            {/* Right */}
             <div className="col-4 text-center align-center d-flex flex-column justify-content-center" style={{ columnGap: "4rem" }}>
-              <span className=" d-flex  align-center gap-1 pb-3  section__description">
+              <span className=" d-flex  align-center gap-1 pb-3 section__description">
               <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -129,6 +154,7 @@ const CarItem = ({ item }) => {
               </span>
             </div>
           </div>
+          </Col> */}
           
           <Link to={`/cars/sad}`}>
             <button
--- frontend/rentACar/src/pages/CarListing.jsx
+++ frontend/rentACar/src/pages/CarListing.jsx
@@ -7,58 +7,178 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchAllCarData } from "../redux/slices/carDataSlice";
 import Loading from "../components/ui/Loading";
 import { useTranslation } from "react-i18next";
+import { Form } from "react-bootstrap";
+import { vehicleType } from "../components/helper/conversionType";
 
 const CarListing = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
   const { items, status, error } = useSelector((state) => state.carAllData);
   const [sortType, setSortType] = useState("low");
-
-  const handleSortChange = (e) => {
-    setSortType(e.target.value);
-  };
+  const [gearType, setGearType] = useState("all");
+  const [fuelType, setFuelType] = useState("all");
+  const [vehicleType, setVehicleType] = useState("all");
+  const [seatType, setSeatType] = useState("all");
 
   useEffect(() => {
     dispatch(fetchAllCarData());
   }, [dispatch]);
 
+  const handleSortChange = (e) => {
+    setSortType(e.target.value);
+  };
+  const handleGearTypeChange = (e) => {
+    setGearType(e.target.value);
+  };
+  const handleFuelTypeChange = (e) => {
+    setFuelType(e.target.value);
+  };
+  const handleVehicleTypeChange = (e) => {
+    setVehicleType(e.target.value);
+  };
+
   if (status === "LOADING") {
     return <Loading />;
   }
 
-  let sortedItems = [...items];
+  let filteredItems = [...items];
 
-  if (sortType === "low") {
-    sortedItems.sort((a, b) => a.dailyPrice - b.dailyPrice);
-  } else if (sortType === "high") {
-    sortedItems.sort((a, b) => b.dailyPrice - a.dailyPrice);
+  // if (gearType !== "all"){
+  //   filteredItems = filteredItems.filter((item) => item.gearType === gearType)
+  // }
+  // if (seatType !== "all"){
+  //   filteredItems = filteredItems.filter((item) => item.seatType === seatType)
+  // }
+  // if (fuelType !== "all"){
+  //   filteredItems = filteredItems.filter((item) => item.vehicleType === fuelType)
+  // }
+  if (gearType === "MANUAL") {
+    filteredItems = filteredItems.filter((item) => item.gearType === gearType);
+  } else if (gearType === "AUTOMATIC") {
+    filteredItems = filteredItems.filter((item) => item.gearType === gearType);
   }
 
+  if (fuelType === "GASOLINE") {
+    filteredItems = filteredItems.filter((item) => item.fuelType === fuelType);
+  } else if (fuelType === "DIESEL") {
+    filteredItems = filteredItems.filter((item) => item.fuelType === fuelType);
+  } else if (fuelType === "HYBRID") {
+    filteredItems = filteredItems.filter((item) => item.fuelType === fuelType);
+  } else if (fuelType === "ELECTRIC") {
+    filteredItems = filteredItems.filter((item) => item.fuelType === fuelType);
+  }
+
+  if (vehicleType === "SUV") {
+    filteredItems = filteredItems.filter(
+      (item) => item.vehicleType === vehicleType
+    );
+  } else if (vehicleType === "SEDAN") {
+    filteredItems = filteredItems.filter(
+      (item) => item.vehicleType === vehicleType
+    );
+  } else if (vehicleType === "HB") {
+    filteredItems = filteredItems.filter(
+      (item) => item.vehicleType === vehicleType
+    );
+  }
+
+  if (sortType === "low") {
+    filteredItems.sort((a, b) => a.dailyPrice - b.dailyPrice);
+  } else if (sortType === "high") {
+    filteredItems.sort((a, b) => b.dailyPrice - a.dailyPrice);
+  }
+
+  console.log(filteredItems);
   return (
-    <Helmet title={t('cars')}>
-      <CommonSection title={t('cars')} />
+    <Helmet title={t("cars")}>
+      <CommonSection title={t("cars")} />
 
       <section>
         <Container>
           <Row>
-            <Col lg="12">
-              <div className="d-flex align-items-center gap-3 mb-5">
-                <span className="d-flex align-items-center gap-2">
-                  <i className="ri-sort-asc"></i> {t('sort')}
-                </span>
-                <select
-                  className="form-control"
-                  value={sortType}
-                  onChange={handleSortChange}
-                >
-                  <option value="">{t('select')}</option>
-                  <option value="low">{t('ltoh')}</option>
-                  <option value="high">{t('htol')}</option>
-                </select>
-              </div>
+            <Col lg="12" className="mb-5 text-center">
+              <Container>
+              <h4 className="section__title">{t('filter')}</h4>
+                <hr />
+                <Row className="mt-4">
+                  <Col lg="4" md="4" sm="6" className="mb-3">
+                  <div className="d-flex align-items-center gap-3">
+                      <span className="d-flex align-items-center gap-2">
+                      <i className="ri-settings-2-line"></i> {t("gearType")}
+                      </span>
+                      <Form.Select
+                      value={gearType}
+                      onChange={handleGearTypeChange}
+                    >
+                      <option value="all">{t("all")}</option>
+                      <option value="MANUAL">{t("manual")}</option>
+                      <option value="AUTOMATIC">{t("automatic")}</option>
+                    </Form.Select>
+                    </div>
+                    
+                  </Col>
+                  <Col lg="4" md="4" sm="6" className="mb-3">
+                  <div className="d-flex align-items-center gap-3">
+                      <span className="d-flex align-items-center gap-2">
+                      <i className="ri-gas-station-fill"></i> {t("fuelType")}
+                      </span>
+                      <Form.Select
+                      value={fuelType}
+                      onChange={handleFuelTypeChange}
+                    >
+                      <option value="all">{t("all")}</option>
+                      <option value="GASOLINE">{t("gasoline")}</option>
+                      <option value="DIESEL">{t("diesel")}</option>
+                      <option value="HYBRID">{t("hybrid")}</option>
+                      <option value="ELECTRIC">{t("electric")}</option>
+                    </Form.Select>
+                    </div>
+                    
+                  </Col>
+                  <Col lg="4" md="4" sm="6" className="mb-3">
+                    <div className="d-flex align-items-center gap-3">
+                      <span className="d-flex align-items-center gap-2">
+                      <i className="ri-caravan-line"></i> {t("vehicleType")}
+                      </span>
+                      <Form.Select
+                      value={vehicleType}
+                      onChange={handleVehicleTypeChange}
+                    >
+                      <option value="all">{t("all")}</option>
+                      <option value="SUV">{t("suv")}</option>
+                      <option value="SEDAN">{t("sedan")}</option>
+                      <option value="HB">{t("hatchback")}</option>
+                    </Form.Select>
+                    </div>
+
+                    
+                  </Col>
+                </Row>
+                <Col lg="9"></Col>
+                <div className="d-flex justify-content-end mt-4">
+                  <Col lg="3">
+                    <Col lg="12">
+                      <div className="d-flex align-items-center gap-3">
+                        <span className="d-flex align-items-center gap-2">
+                          <i className="ri-sort-asc"></i> {t("sort")}
+                        </span>
+                        <select
+                          className="form-control"
+                          value={sortType}
+                          onChange={handleSortChange}
+                        >
+                          <option value="">{t("select")}</option>
+                          <option value="low">{t("ltoh")}</option>
+                          <option value="high">{t("htol")}</option>
+                        </select>
+                      </div>
+                    </Col>
+                  </Col>
+                </div>
+              </Container>
             </Col>
 
-            {sortedItems.map((item) => (
+            {filteredItems.map((item) => (
               <CarItem item={item} key={item.id} />
             ))}
           </Row>
