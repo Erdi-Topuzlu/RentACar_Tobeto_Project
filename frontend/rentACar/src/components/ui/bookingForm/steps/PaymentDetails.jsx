e44b652135f5bdda57023497ae8ae4b3cf8568a4
Halismelih1 <hhlsabkk@icloud.com>
2024-02-04 18:11:03 +0300

    Payment detail review completed

--- frontend/rentACar/src/components/ui/bookingForm/steps/PaymentDetails.jsx
+++ frontend/rentACar/src/components/ui/bookingForm/steps/PaymentDetails.jsx
@@ -3,36 +3,42 @@ import PaymentForm from "./creditCard/PaymentForm";
 
 export function PaymentDetails({ steps, activeStep, setActiveStep }) {
   return (
-    <div>
+    <>
+     <div>
       <PaymentForm />
+      
+    </div>
+    <div>
       <div className="row card-deck mb-3 text-center">
      
-      <div className="d-flex align-items-center justify-content-between mt-4">
-        {activeStep !== steps.length - 1 && (
-          <Button
-            disabled={activeStep === 0}
-            color="secondary"
-            onClick={() => setActiveStep(activeStep - 1)}
-          >
-            Previous
-          </Button>
-        )}
-        {
-          <div className="d-flex justify-content-end">
-            {activeStep !== steps.length - 1 && (
-              <Button
-                className="form__btn"
-                onClick={() => setActiveStep(activeStep + 1)}
-              >
-                Next
-              </Button>
-            )}
-          </div>
-        }
-      </div>
-    </div>
+     <div className="d-flex align-items-center justify-content-between mt-4">
+       {activeStep !== steps.length - 1 && (
+         <Button
+           disabled={activeStep === 0}
+           color="secondary"
+           onClick={() => setActiveStep(activeStep - 1)}
+         >
+           Previous
+         </Button>
+       )}
+       {
+         <div className="d-flex justify-content-end">
+           {activeStep !== steps.length - 1 && (
+             <Button
+               className="form__btn"
+               onClick={() => setActiveStep(activeStep + 1)}
+             >
+               Next
+             </Button>
+           )}
+         </div>
+       }
+     </div>
+   </div>
     </div>
 
+    </>
+   
 
   )
 }
--- frontend/rentACar/src/components/ui/bookingForm/steps/creditCard/PaymentForm.jsx
+++ frontend/rentACar/src/components/ui/bookingForm/steps/creditCard/PaymentForm.jsx
@@ -3,162 +3,164 @@ import Cards from 'react-credit-cards-2';
 import 'react-credit-cards-2/dist/es/styles-compiled.css';
 
 import {
-    formatCreditCardNumber,
-    formatCVC,
-    formatExpirationDate,
-    formatFormData
+  formatCreditCardNumber,
+  formatCVC,
+  formatExpirationDate,
+  formatFormData
 } from "./utils";
 import { Col, Container, Row } from 'reactstrap';
 
 const PaymentForm = ({ steps, activeStep, setActiveStep }) => {
-    const [state, setState] = useState({
-        number: "",
-        name: "",
-        expiry: "",
-        cvc: "",
-        issuer: "",
-        focused: "",
-        formData: null
-    });
-    
+  const [state, setState] = useState({
+    number: "",
+    name: "",
+    expiry: "",
+    cvc: "",
+    issuer: "",
+    focused: "",
+    formData: null
+  });
 
 
-    const formRef = useRef(null);
 
-    const handleCallback = ({ issuer }, isValid) => {
-        if (isValid) {
-            setState(prevState => ({ ...prevState, issuer }));
-        }
-    };
+  const formRef = useRef(null);
 
-    const handleInputFocus = ({ target }) => {
-        setState(prevState => ({ ...prevState, focused: target.name }));
-    };
+  const handleCallback = ({ issuer }, isValid) => {
+    if (isValid) {
+      setState(prevState => ({ ...prevState, issuer }));
+    }
+  };
 
-    const handleInputChange = ({ target }) => {
-        let value = target.value;
+  const handleInputFocus = ({ target }) => {
+    setState(prevState => ({ ...prevState, focused: target.name }));
+  };
 
-        if (target.name === "number") {
-            value = formatCreditCardNumber(value);
-        } else if (target.name === "expiry") {
-            value = formatExpirationDate(value);
-        } else if (target.name === "cvc") {
-            value = formatCVC(value);
-        } else if (target.name === "name") {
-            value = value.replace(/[^A-Za-z ]/g, ''); // Sadece harfleri ve boşlukları kabul et
-        }
+  const handleInputChange = ({ target }) => {
+    let value = target.value;
 
-        setState(prevState => ({ ...prevState, [target.name]: value }));
-        localStorage.setItem("PaymentFormData", JSON.stringify(state));
+    if (target.name === "number") {
+      value = formatCreditCardNumber(value);
+    } else if (target.name === "expiry") {
+      value = formatExpirationDate(value);
+    } else if (target.name === "cvc") {
+      value = formatCVC(value);
+    } else if (target.name === "name") {
+      value = value.replace(/[^A-Za-z ]/g, ''); // Sadece harfleri ve boşlukları kabul et
+    }
 
-    };
+    setState(prevState => ({ ...prevState, [target.name]: value }));
+    localStorage.setItem("PaymentFormData", JSON.stringify(state));
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        
-        const { issuer } = state;
-        const formData = [...e.target.elements]
-            .filter(d => d.name)
-            .reduce((acc, d) => {
-                acc[d.name] = d.value;
-                return acc;
-            }, {});
+  };
 
-        setState(prevState => ({ ...prevState, formData }));
-        console.log("Form Data:", state);
-        formRef.current.reset();
-    };
+  const handleSubmit = e => {
+    e.preventDefault();
 
-    const { name, number, expiry, cvc, focused, issuer, formData } = state;
+    const { issuer } = state;
+    const formData = [...e.target.elements]
+      .filter(d => d.name)
+      .reduce((acc, d) => {
+        acc[d.name] = d.value;
+        return acc;
+      }, {});
 
-    return (
-        
-            <Container>
-    <Row>
-      {/* Left Column */}
-      <Col md="6">
-        <div key="Payment">
-          <div className="App-payment">
-            <form ref={formRef} onSubmit={handleSubmit}>
-              <div className="form-group mb-4 mt-4">
-                <input
-                  type="text"
-                  id="number"
-                  name="number"
-                  className="form-control"
-                  placeholder="Enter Card Number"
-                  pattern="\d*"
-                  maxLength="16"
-                  required
-                  onChange={handleInputChange}
-                  onFocus={handleInputFocus}
-                />
-              </div>
-              <div className="form-group mb-4">
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="form-control"
-                  placeholder="Enter Name"
-                  pattern="[A-Za-z ]+"
-                  required
-                  onChange={handleInputChange}
-                  onFocus={handleInputFocus}
-                />
-              </div>
-              <div className="row">
-                <div className="col-6">
+    setState(prevState => ({ ...prevState, formData }));
+    console.log("Form Data:", state);
+    formRef.current.reset();
+  };
+
+  const { name, number, expiry, cvc, focused, issuer, formData } = state;
+
+  return (
+
+    <Container>
+      <Row>
+        {/* Left Column */}
+        <Col md="6" className='mt-2'>
+          <div key="Payment">
+            <div className="App-payment">
+              <form ref={formRef} onSubmit={handleSubmit}>
+                <div className="form-group mb-4 mt-4">
                   <input
                     type="text"
-                    id="expiry"
-                    name="expiry"
+                    id="number"
+                    name="number"
                     className="form-control"
-                    placeholder="MM/YY"
-                    maxLength="4"
+                    placeholder="Enter Card Number"
+                    pattern="\d*"
+                    maxLength="16"
                     required
                     onChange={handleInputChange}
                     onFocus={handleInputFocus}
                   />
                 </div>
-                <div className="col-6">
+                <div className="form-group mb-4">
                   <input
                     type="text"
-                    id="cvc"
-                    name="cvc"
+                    id="name"
+                    name="name"
                     className="form-control"
-                    placeholder="CVC"
-                    pattern="\d{3,4}"
-                    maxLength="4"
+                    placeholder="Enter Name"
+                    pattern="[A-Za-z ]+"
                     required
                     onChange={handleInputChange}
                     onFocus={handleInputFocus}
                   />
                 </div>
-              </div>
-            </form>
+                <div className="row">
+                  <div className="col-6">
+                    <input
+                      type="text"
+                      id="expiry"
+                      name="expiry"
+                      className="form-control"
+                      placeholder="MM/YY"
+                      maxLength="4"
+                      required
+                      onChange={handleInputChange}
+                      onFocus={handleInputFocus}
+                    />
+                  </div>
+                  <div className="col-6">
+                    <input
+                      type="text"
+                      id="cvc"
+                      name="cvc"
+                      className="form-control"
+                      placeholder="CVC"
+                      pattern="\d{3,4}"
+                      maxLength="4"
+                      required
+                      onChange={handleInputChange}
+                      onFocus={handleInputFocus}
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
-      </Col>
+        </Col>
 
-      {/* Right Column */}
-      <Col md="6">
-        <div className="App-payment">
-          <Cards
-            number={number}
-            name={name}
-            expiry={expiry}
-            cvc={cvc}
-            focused={focused}
-            callback={handleCallback}
-          />
-        </div>
-      </Col>
-    </Row>
-  </Container>
-       
-                   
-    );
+        {/* Right Column */}
+        <Col md="6" className='mt-4'>
+          <div className="App-payment">
+            <Cards
+              number={number}
+              name={name}
+              expiry={expiry}
+              cvc={cvc}
+              focused={focused}
+              callback={handleCallback}
+            />
+          </div>
+        </Col>
+      </Row>
+      <hr style={{ margin: "60px 0 30px" }} />
+
+    </Container>
+
+
+  );
 
 };
 
