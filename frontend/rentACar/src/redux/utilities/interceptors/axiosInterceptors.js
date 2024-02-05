6dd6ea5aa66e218d637c04710e7e93c745f7cba9
halilkrkn <halilkrkn000@gmail.com>
2024-02-05 22:59:13 +0300

    Changed users access token with Refresh Token in the interceptor

--- frontend/rentACar/src/redux/utilities/interceptors/axiosInterceptors.js
+++ frontend/rentACar/src/redux/utilities/interceptors/axiosInterceptors.js
@@ -39,55 +39,46 @@ axiosInstance.interceptors.response.use(
     const originalRequest = error.config;
 
     // Hata 401 ise ve refresh token varsa
-    if (error.response.status === 403 && !originalRequest._retry) {
+    if ((error.response.status === 403 || error.response.status === 401) && !originalRequest._retry) {
       originalRequest._retry = true;
-      console.log("Şuan Buradasın 1");
 
       try {
-        // Refresh token isteği
+
         const refreshToken = localStorage.getItem("refresh_token");
+
         console.log(refreshToken);
-        console.log("Şuan Buradasın 2");
-        // const headers = {
-        //   Authorization: `Bearer ${refreshToken}`,
-        // };
+        const headers = {
+          Authorization: `Bearer ${refreshToken}`,
+        };
         const refreshResponse = await fetch(
           "http://localhost:8080/api/v1/auth/refresh-token",
           {
             method: "POST",
-            headers: {
-              Authorization: `Bearer ${refreshToken}`,
-            },
+            headers: headers
           }
         );
 
         const refreshData = await refreshResponse.json(); // Yanıtı JSON formatına dönüştür
         console.log(refreshData);
 
-        // Yeni erişim tokenını kaydet
         const newAccessToken = refreshData.access_token;
         localStorage.removeItem("access_token");
         console.log(newAccessToken);
         localStorage.setItem("access_token", newAccessToken);
 
-        // Yeni erişim tokenını ekleyerek orijinal isteği tekrar dene
         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-        console.log("Şuan Buradasın 3");
         if(originalRequest.headers.Authorization){
           toastSuccess("Kullanıcının Access Tokenı Değişti")
         }
         return axiosInstance(originalRequest);
       } catch (refreshError) {
         // Refresh token hatası
-        console.log("Şuan Buradasın 4");
         console.error("Refresh token error:", refreshError);
-        // Burada istediğiniz şekilde işlem yapabilirsiniz, örneğin kullanıcıyı logout yapabilirsiniz.
-        localStorage.clear(); // Kullanıcıyı logout yap
-        window.location.href = "/login"; // Login sayfasına yönlendir
+        localStorage.clear(); 
+        window.location.href = "/login";
         return Promise.reject(refreshError);
       }
     }
-    console.log("Şuan Buradasın 5");
     toastWarning("Bir hata oluştu, lütfen tekrar deneyin.")
     return Promise.reject(error);
   }
