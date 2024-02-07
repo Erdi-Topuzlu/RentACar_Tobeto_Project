4d5c4a727e6608cd754c793845449534b4794fca
halilkrkn <halilkrkn000@gmail.com>
2024-02-08 01:41:55 +0300

    Protected Routed Bugs Fixed

--- frontend/rentACar/src/routes/ProtectedRoute.jsx
+++ frontend/rentACar/src/routes/ProtectedRoute.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
-
 export default function ({ children }) {
   const navigate = useNavigate();
   const { details } = useSelector((state) => state.userDetail);
@@ -11,10 +10,15 @@ export default function ({ children }) {
   if (!details) {
     return <Navigate to="/login" />;
   }
-  
+
   if (details.role) {
     return <Outlet />;
   }
+
+  if (details.role != "ADMIN" || details.role != "USER") {
+    return <Navigate to="/home" />;
+  }
+
 
 
   return children;
