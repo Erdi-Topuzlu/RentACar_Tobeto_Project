61eae1bb2cbf5c10fa1051d58b6785249a29a13e
Halismelih1 <hhlsabkk@icloud.com>
2024-02-09 01:30:52 +0300

    Car filter edited

--- frontend/rentACar/src/components/ui/FindCarFrom.jsx
+++ frontend/rentACar/src/components/ui/FindCarFrom.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
 import "../../styles/find-car-form.css";
-import "../../styles/find-car-form.css";
 import { Form, FormGroup } from "reactstrap";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 import { toastWarning } from "../../service/ToastifyService";
 
 const FindCarForm = () => {
-  const { items, status, error } = useSelector((state) => state.carAllData);
+  const { items } = useSelector((state) => state.carAllData);
   const [selectedBrand, setSelectedBrand] = useState("");
   const [selectedModel, setSelectedModel] = useState("");
   const { t } = useTranslation();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const selectBrands = []; 
+  const selectModels = []; 
 
   const handleBrandChange = (event) => {
-    setSelectedBrand(event.target.value);
-    setSelectedModel(""); 
+    const brandName = event.target.value;
+    setSelectedBrand(brandName);
+    setSelectedModel("");
   };
 
   const handleModelChange = (event) => {
@@ -26,57 +27,68 @@ const FindCarForm = () => {
 
   const handleFindCar = () => {
     if (!selectedBrand && !selectedModel) {
-     toastWarning("Lütfen bir marka ve model seçiniz.");
-      // Her iki input da seçilmişse yönlendirme yapılacak burada
+      toastWarning("Lütfen bir marka ve model seçiniz.");
     } else {
       localStorage.setItem("selectedBrand", selectedBrand);
       localStorage.setItem("selectedModel", selectedModel);
       navigate("/findCarResult");
-
     }
-  }
-
+  };
 
   return (
-    
-    <Form className="form ">
-      <div className=" d-flex align-items-center justify-content-between flex-wrap">
+    <Form className="form">
+      <div className="d-flex align-items-center justify-content-between flex-wrap">
         {/* Brand */}
         <FormGroup className="select__group">
-          <select className="mt-4"
+          <select
+            className="mt-4"
             value={selectedBrand}
             onChange={handleBrandChange}
           >
             <option value="">{t("Brand")}</option>
-            {items.map((car, i) => (
-              <option key={i} value={car.modelId?.brandId?.name}>
-                {car.modelId?.brandId?.name}
-              </option>
-            ))}
+            {items.map((car, i) => {
+              const brandName = car.modelId?.brandId?.name;
+              if (!selectBrands.includes(brandName)) {
+                selectBrands.push(brandName);
+                return (
+                  <option key={i} value={brandName}>
+                    {brandName}
+                  </option>
+                );
+              }
+              return null;
+            })}
           </select>
         </FormGroup>
+        
 
         {/* Model */}
         <FormGroup className="select__group">
           <select
-           className="mt-4"
+            className="mt-4"
             value={selectedModel}
             onChange={handleModelChange}
             disabled={!selectedBrand}
           >
             <option value="">{t("Model")}</option>
-            {/* Markaya göre filtreleme*/}
             {items
-              .filter(car => car.modelId.brandId.name === selectedBrand)
-              .map((car, index) => (
-                <option key={index} value={car.modelId.name}>
-                  {car.modelId.name}
-                </option>
-              ))}
+              .filter((car) => car.modelId.brandId.name === selectedBrand)
+              .map((car, index) => {
+                const modelName = car.modelId.name;
+                if (!selectModels.includes(modelName)) {
+                  selectModels.push(modelName);
+                  return (
+                    <option key={index} value={modelName}>
+                      {modelName}
+                    </option>
+                  );
+                }
+                return null;
+              })}
           </select>
         </FormGroup>
 
-        <FormGroup className="form__group  mt-4">
+        <FormGroup className="form__group mt-4">
           <button className="btn find__car-btn" onClick={handleFindCar}>
             {t("findBtn")}
           </button>
