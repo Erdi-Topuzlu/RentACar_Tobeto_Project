fad1f6153c2d4cc985ad0931d0a86ad81c3ba78c
Erdi Topuzlu <erditpzl@gmail.com>
2024-02-10 18:26:38 +0300

    Model Bug Fixed in Admin Panel

--- backend/src/main/java/com/tobeto/RentACar/entities/concretes/model/Model.java
+++ backend/src/main/java/com/tobeto/RentACar/entities/concretes/model/Model.java
@@ -6,15 +6,20 @@ import com.tobeto.RentACar.entities.concretes.brand.Brand;
 import com.tobeto.RentACar.entities.concretes.car.Car;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.util.List;
 
 @Table(name = "models")
 @Entity
 @Data
-public class Model extends BaseEntity {
+public class Model {
 
-
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "my_sequence")
+    @SequenceGenerator(name = "my_sequence", sequenceName = "my_sequence", allocationSize = 1, initialValue = 10000)
+    private int id;
 
     @Column(name = "name")
     private String name;
