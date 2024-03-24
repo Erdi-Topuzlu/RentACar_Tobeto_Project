43d465b9b69de86aeb0d7b4d39babd1ccc2abce5
Halismelih1 <hhlsabkk@icloud.com>
2024-03-25 02:44:19 +0300

    deneme

--- backend/src/main/java/com/tobeto/RentACar/core/utilities/EmailUtils.java
+++ backend/src/main/java/com/tobeto/RentACar/core/utilities/EmailUtils.java
@@ -5,11 +5,9 @@ public class EmailUtils {
         return "Hello " + name + ",\n\nYour new account has been created. Please click the link below to verify your account. \n\n" +
                 getVerificationUrl(host, token) + "\n\nThe support Team";
     }
-
     public static String getVerificationUrl(String host, String token) {
         return host + "/api/v1/auth/confirm-account?token=" + token;
     }
-
     public static String getResetPasswordUrl(String token) {
         return "http://localhost:5173/reset-password";
     }
